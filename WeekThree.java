ad7dc2c96fad3bd47e1a5dbc58131fbc271880d7
penpro <penumbraproductionsllc@gmail.com>
2025-10-16 20:24:13 -0700

    done i think

--- WeekThree.java
+++ WeekThree.java
@@ -4,6 +4,8 @@ public class WeekThree {
     public static void main(String[] args) {
 
         System.out.println(recSumToN(4));
+        System.out.println(recSumToN2(4));
+        System.out.println(recFibonacci(5));
 
     }
 
@@ -30,10 +32,15 @@ public class WeekThree {
 
     }
 
+    public static int recSumToN2(int n){
+        if(n < 0) throw new IllegalArgumentException("n < 0");
+        return (n == 0) ? 0 : n+recSumToN(n-1);
+    }
+
     public static int recFibonacci(int n){
         if(n == 0) return 0;
-
         if(n == 1) return 1;
+        return recFibonacci(n-1) + recFibonacci(n-2);
 
     }
 
